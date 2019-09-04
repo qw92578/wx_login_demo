74cbbb73eae5eb75ddcd736f2d7d8499bc4b62ef
fanggq <qw92578@gmail.com>
2019-09-04 17:51:08 +0800

    项目结构调整

---
+++ src/main/java/com/fgq/wxlogin/demo/pojo/WeChatUserInfo.java
@@ -0,0 +1,27 @@
+package com.fgq.wxlogin.demo.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @Package: com.fgq.wxlogin.demo.pojo
+ * @ClassName: WeChatUserInfo
+ * @Author: fgq
+ * @Description:
+ * @Date: 2019/9/4 17:18
+ */
+
+public class WeChatUserInfo implements Serializable {
+    String openid;
+    String nickname;
+    Integer sex;
+    String province;
+    String city;
+    String country;
+    String headimgurl;
+    String privilege;
+    String unionid;
+
+
+
+
+}
