22227d7df2d9a1397f11972e04ff1982746b3b5a
fanggq <qw92578@gmail.com>
2019-09-10 14:39:39 +0800

    pom文件修改

---
+++ src/test/java/com/fgq/wxlogin/demo/Test.java
@@ -0,0 +1,19 @@
+package com.fgq.wxlogin.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Package: com.fgq.wxlogin.demo
+ * @ClassName: Test
+ * @Author: fgq
+ * @Description:
+ * @Date: 2019/9/6 8:55
+ */
+public class Test {
+    public static void main(String[] args) {
+        List list = new ArrayList<>();
+        list.add("hello");
+        list.add("world");
+    }
+}
